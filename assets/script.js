421e9417b04ec9d48bc2ed0dd52b61195d8cbbe1
Trevor-McGuire <trevor.mcguire.candidate@gmail.com>
2023-06-11 16:30:13 -0500

    to more commit volume

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,15 @@ function readLocalStorage(projectName) {
     var project = projectName.toString()
     return localStorage.project
 }
+function createTableRow(dueDate) {
+    var tr = document.createElement("li")
+
+    if (dueDate < todaysDate) {
+        tr.className = "not-late"
+    } else if (dueDate = todaysDate) {
+        tr.className = "due-today"
+    } else {
+        tr.className = "late"
+    }
+    
+}
